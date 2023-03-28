c4a3b2877cdb22a17cc7e9e54295b00424950bef
zarif007 <zarifhuq786@gmail.com>
2023-03-29 01:26:03 +0600

    Adding the layout

---
+++ fakeapi/src/Providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { SessionProvider } from 'next-auth/react'
+import { ThemeProvider } from 'next-themes'
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+    <SessionProvider>{children}</SessionProvider>
+  </ThemeProvider>
+}
+
+export default Providers
---
+++ fakeapi/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import Navbar from '@/components/Navbar'
+import { cn } from '@/lib/utils'
+import Providers from '@/Providers'
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={cn('bg-white text-slate-900 antialiased', inter.style)}>
+      <body className='min-h-screen bg-slate-50 dark:bg-slate-900 antialiased'>
+        <Providers>
+          <Navbar />
+          {children}
+          
+        </Providers>
+
+        {/* Allowing more hights for mobile devices */}
+        <div className='h-40 md:hidden' />
+      </body>
+    </html>
+  )
+}
