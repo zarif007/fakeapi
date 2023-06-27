7b082be45210f54702bcc591a81cfc829ef095d3
zarif007 <zarifhuq786@gmail.com>
2023-06-28 00:46:33 +0600

    [ADDED] backend

---
+++ fakeapi/src/app/(generator)/workspace/[id]/page.tsx
@@ -0,0 +1,44 @@
+import GeneratorTabs from "@/components/generator/Generator.Tabs";
+import GnTopbar from "@/components/generator/GnTopbar";
+import { db } from "@/lib/db";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React, { FC } from "react";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+
+interface PageParams {
+  params: {
+    id: string;
+  };
+}
+
+const Generator = async ({ params }: PageParams) => {
+  if (!params.id) notFound();
+
+  const project = await db.project.findFirst({
+    where: {
+      id: params.id,
+    },
+  });
+
+  // const router = useRouter()
+
+  return (
+    <div className="w-full px-2 md:px-12">
+      {/* @ts-expect-error server component */}
+      <GnTopbar project={project} />
+
+      <Link
+        href="/dashboard"
+        className="flex justify-end text-blue-500 space-x-1 items-center cursor-pointer font-semibold text-md"
+      >
+        <AiOutlineArrowLeft className="w-6 h-6" />
+        <p>Back</p>
+      </Link>
+
+      <GeneratorTabs project={project} />
+    </div>
+  );
+};
+
+export default Generator;
