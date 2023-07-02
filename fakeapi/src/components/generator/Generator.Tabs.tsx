55180f3eeb1a9f3095258a0c00d6411363bf9b61
zarif007 <zarifhuq786@gmail.com>
2023-07-03 01:28:15 +0600

    [CHANGED]: file naming

--- fakeapi/src/components/generator/Generator.Tabs.tsx
+++ fakeapi/src/components/generator/Generator.Tabs.tsx
@@ -4,27 +4,31 @@ import React from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/Tabs';
 import { GiPowerGenerator } from 'react-icons/gi';
 import { MdDataObject } from 'react-icons/md';
-import GenerateSchema from './GenerateSchema.page';
+import GenerateSchema from './GenerateSchema.Page';
 import SimpleBar from 'simplebar-react';
 import GeneratorSettings from './GeneratorSettings.Page';
 import { AiOutlineSetting } from 'react-icons/ai';
 import { VscGistSecret } from 'react-icons/vsc';
 
 const GeneratorTabs = ({ project }: { project: any }) => {
+  const styles = {
+    iconStyle: 'h-5 w-5',
+    tabsTriggerStyle: 'py-2 flex space-x-2',
+  };
   return (
     <Tabs defaultValue="generator" className="overflow-x-auto">
       <TabsList>
-        <TabsTrigger value="generator" className="py-2 flex space-x-2">
-          <GiPowerGenerator className="h-5 w-5" /> <p>Generator</p>
+        <TabsTrigger value="generator" className={styles.tabsTriggerStyle}>
+          <GiPowerGenerator className={styles.iconStyle} /> <p>Generator</p>
         </TabsTrigger>
-        <TabsTrigger value="demo" className="py-2 flex space-x-2">
-          <MdDataObject className="h-5 w-5" /> <p>Demo</p>
+        <TabsTrigger value="demo" className={styles.tabsTriggerStyle}>
+          <MdDataObject className={styles.iconStyle} /> <p>Demo</p>
         </TabsTrigger>
-        <TabsTrigger value="credentials" className="py-2 flex space-x-2">
-          <VscGistSecret className="h-5 w-5" /> <p>Credentials</p>
+        <TabsTrigger value="credentials" className={styles.tabsTriggerStyle}>
+          <VscGistSecret className={styles.iconStyle} /> <p>Credentials</p>
         </TabsTrigger>
-        <TabsTrigger value="settings" className="py-2 flex space-x-2">
-          <AiOutlineSetting className="h-5 w-5" /> <p>Settings</p>
+        <TabsTrigger value="settings" className={styles.tabsTriggerStyle}>
+          <AiOutlineSetting className={styles.iconStyle} /> <p>Settings</p>
         </TabsTrigger>
       </TabsList>
       <TabsContent value="generator" className="h-full">
