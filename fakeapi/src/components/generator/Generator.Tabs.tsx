e15fc882ae1cae7fe52533dd8606a64341c12c69
zarif007 <zarifhuq786@gmail.com>
2023-07-04 01:09:58 +0600

    [FIXED]: typo

--- fakeapi/src/components/generator/Generator.Tabs.tsx
+++ fakeapi/src/components/generator/Generator.Tabs.tsx
@@ -9,28 +9,35 @@ import SimpleBar from 'simplebar-react';
 import GeneratorSettings from './GeneratorSettings.Page';
 import { AiOutlineSetting } from 'react-icons/ai';
 import { VscGistSecret } from 'react-icons/vsc';
+import { GrDocumentTest, GrTest } from 'react-icons/gr';
+import { TbTestPipe } from 'react-icons/tb';
 
 const GeneratorTabs = ({ project }: { project: any }) => {
   const styles = {
-    iconStyle: `h-5 w-5`,
-    tabsTriggerStyle: `py-2 flex space-x-2`,
+    iconStyle: 'h-5 w-5',
+    tabTriggerStyle: 'py-2 flex space-x-2',
   };
   return (
-    <Tabs defaultValue="generator" className="overflow-x-auto">
-      <TabsList>
-        <TabsTrigger value="generator" className={styles.tabsTriggerStyle}>
-          <GiPowerGenerator className={styles.iconStyle} /> <p>Generator</p>
-        </TabsTrigger>
-        <TabsTrigger value="demo" className={styles.tabsTriggerStyle}>
-          <MdDataObject className={styles.iconStyle} /> <p>Demo</p>
-        </TabsTrigger>
-        <TabsTrigger value="credentials" className={styles.tabsTriggerStyle}>
-          <VscGistSecret className={styles.iconStyle} /> <p>Credentials</p>
-        </TabsTrigger>
-        <TabsTrigger value="settings" className={styles.tabsTriggerStyle}>
-          <AiOutlineSetting className={styles.iconStyle} /> <p>Settings</p>
-        </TabsTrigger>
-      </TabsList>
+    <Tabs defaultValue="generator" className="">
+      <SimpleBar>
+        <TabsList>
+          <TabsTrigger value="generator" className={styles.tabTriggerStyle}>
+            <GiPowerGenerator className={styles.iconStyle} /> <p>Generator</p>
+          </TabsTrigger>
+          <TabsTrigger value="demo" className={styles.tabTriggerStyle}>
+            <MdDataObject className={styles.iconStyle} /> <p>Demo</p>
+          </TabsTrigger>
+          <TabsTrigger value="test" className={styles.tabTriggerStyle}>
+            <TbTestPipe className={styles.iconStyle} /> <p>Test</p>
+          </TabsTrigger>
+          <TabsTrigger value="credentials" className={styles.tabTriggerStyle}>
+            <VscGistSecret className={styles.iconStyle} /> <p>Credentials</p>
+          </TabsTrigger>
+          <TabsTrigger value="settings" className={styles.tabTriggerStyle}>
+            <AiOutlineSetting className={styles.iconStyle} /> <p>Settings</p>
+          </TabsTrigger>
+        </TabsList>
+      </SimpleBar>
       <TabsContent value="generator" className="h-full">
         <SimpleBar>
           <GenerateSchema project={project} />
@@ -38,8 +45,11 @@ const GeneratorTabs = ({ project }: { project: any }) => {
       </TabsContent>
       <TabsContent value="demo"></TabsContent>
       <TabsContent value="credentials"></TabsContent>
+      <TabsContent value="test"></TabsContent>
       <TabsContent value="settings">
-        <GeneratorSettings />
+        <SimpleBar>
+          <GeneratorSettings />
+        </SimpleBar>
       </TabsContent>
     </Tabs>
   );
